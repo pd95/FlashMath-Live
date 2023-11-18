fc67043d5761750566ffb12dd65b8b8e8c2f6e20
Philipp <pd95@users.noreply.github.com>
2023-11-18 18:07:49 +0100

    Initial Commit

---
+++ FlashMath/FlashMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashMathApp.swift
+//  FlashMath
+//
+//  Created by Philipp on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
